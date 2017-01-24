e264fdf45630904088cbdcb27d3e4a3211783ff1
ECE 437L <ece437l@ecn.purdue.edu>
2017-01-24 14:45:32 -0500

    Introduce CPU tracker to single cycle repo

--- testbench/system_tb.sv
+++ testbench/system_tb.sv
@@ -35,6 +35,50 @@ module system_tb;
   // dut
 `ifndef MAPPED
   system                              DUT (CLK,nRST,syif);
+
+  // CPU Tracker. Uncomment and change signal names to enable.
+  /*
+  cpu_tracker                         cpu_track0 (
+    // No need to change this
+    .CLK(DUT.CPU.DP.CLK),
+    // Since single cycle, this is just PC enable
+    .wb_stall(~DUT.CPU.DP.pc0_en),
+    // The 'funct' portion of an instruction. Must be of funct_t type
+    .funct(DUT.CPU.DP.funct),
+    // The 'opcode' portion of an instruction. Must be of opcode_t type
+    .opcode(DUT.CPU.DP.op_code),
+    // The 'rs' portion of an instruction
+    .rs(DUT.CPU.DP.rs),
+    // The 'rt' portion of an instruction
+    .rt(DUT.CPU.DP.rt),
+    // The final selected wsel
+    .wsel(DUT.CPU.DP.rfif.wsel),
+    // Make sure the interface (dpif) matches your name
+    .instr(DUT.CPU.DP.dpif.imemload),
+    // Connect the PC to this
+    .pc(DUT.CPU.DP.PC0),
+    // Connect the next PC value (the next registered value) here
+    .npc(DUT.CPU.DP.pc_selected),
+    // The final imm/shamt signals
+    // This means it should already be shifted/extended/whatever
+    .imm(DUT.CPU.DP.imm_shamt_out),
+    .shamt(DUT.CPU.DP.imm_shamt_out),
+     .lui(DUT.CPU.DP.imm),
+    // The branch target (aka offset added to npc)
+    .branch_addr(DUT.CPU.DP.pc_branch),
+    // Make sure the interface (dpif) matches your name
+    .dat_addr(DUT.CPU.DP.dpif.dmemaddr),
+    // Make sure the interface (dpif) matches your name
+    .store_dat(DUT.CPU.DP.dpif.dmemstore),
+    // Make sure the interface (dpif) matches your name
+    .reg_dat(DUT.CPU.DP.rfif.wdat),
+    // Make sure the interface (dpif) matches your name
+    .load_dat(DUT.CPU.DP.dpif.dmemload),
+    // Make sure the interface (dpif) matches your name
+    .dhit(DUT.CPU.DP.dpif.dhit)
+  );
+  */
+
 `else
   system                              DUT (,,,,//for altera debug ports
     CLK,
